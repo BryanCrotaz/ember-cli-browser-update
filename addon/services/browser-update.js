ff64b990ce98df4abdcfbcacabbca110ac847b97
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-04-13 02:46:14 +0100

    Fixed binding bug

--- addon/services/browser-update.js
+++ addon/services/browser-update.js
@@ -4,7 +4,6 @@ import injectScript from 'ember-inject-script';
 export default Ember.Service.extend(Ember.Evented, {
 	inject: function () {
 		var config = this.get('_config');
-		// send config to browser-update script as global variable
 		window.$buoop = config;
 		this._injectAfterDelay(config.delay);		
 	},
@@ -25,17 +24,18 @@ export default Ember.Service.extend(Ember.Evented, {
 		delay = delay || 100;
 		Ember.run.later(this, function() {
 			// inject the browser update script
-			var url = "//browserupdate.org/update.js";
+			var url = "//browser-update.org/update.js";
 			injectScript(url);
 		}, delay);
 	},
 
 	_config: function() {
 		var config = this.get('config');
+		// send config to browser-update script as global variable
 		config.vs = config.vs || {i:9,f:2,o:9.63,s:2,c:10};
-		config.onshow = this.show;
-		config.onclick = this.click;
-		config.onclose = this.close;
+		config.onshow = this.show.bind(this);
+		config.onclick = this.click.bind(this);
+		config.onclose = this.close.bind(this);
 		return config;
-	}
+	}.property()
 });
