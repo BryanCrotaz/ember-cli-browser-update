3c3044da18f3945486c8093ce28814ae0d9e3e1d
Jan Dillmann <j@n-dillmann.de>
2016-01-12 10:52:10 +0100

    Refactor for Ember 2.x

---
+++ addon/instance-initializers/browser-update.js
@@ -0,0 +1,10 @@
+export function initialize(appInstance) {
+  let service = appInstance.container.lookup('service:browser-update');
+
+  service.inject();
+}
+
+export default {
+  name: 'browser-update',
+  initialize
+}
